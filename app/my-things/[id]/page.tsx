8253303dfecd8629b3efb49c98e8faf33eaf61a9
Suman Sourabh <sumsourabh14@gmail.com>
2025-07-29 15:02:27 +0530

    updated thing page ui

--- app/my-things/[id]/page.tsx
+++ app/my-things/[id]/page.tsx
@@ -20,28 +20,26 @@ const ThingPage = ({ params }: Props) => {
   const daysLeft = getDaysLeft(deadline);
 
   return (
-    <main className="font-sans flex items-center justify-center px-8 py-20 lg:py-40">
-      <div className="max-w-6xl w-full rounded-xl shadow-md p-6 mx-auto space-y-4">
-        <div className="flex gap-16 items-center flex-col md:flex-row">
-          <div className="text-center flex-1/2">
-            <h1 className="text-8xl sm:text-9xl font-bold">{daysLeft}</h1>
-            <p className="text-xl">days to go</p>
-          </div>
-          <div className="space-y-4">
-            <h2 className="text-3xl font-semibold text-muted-foreground">
-              {title}
-            </h2>
-            {imageUrl && (
-              <img
-                src={imageUrl}
-                alt="Uploaded"
-                className="w-full max-h-[400px] object-contain rounded-lg border border-neutral-300 dark:border-neutral-700 shadow-sm"
-              />
-            )}
-          </div>
+    <section className="font-sans flex items-center justify-center px-8 py-20">
+      <section className="flex gap-16 items-center flex-col md:flex-row">
+        <div className="text-center flex-1/2">
+          <h1 className="text-8xl sm:text-9xl font-bold">{daysLeft}</h1>
+          <p className="text-xl">days to go</p>
         </div>
-      </div>
-    </main>
+        <div className="space-y-4">
+          <h2 className="text-3xl font-semibold text-muted-foreground">
+            {title}
+          </h2>
+          {imageUrl && (
+            <img
+              src={imageUrl}
+              alt="Uploaded"
+              className="w-full max-h-[400px] object-contain rounded-lg border border-neutral-300 dark:border-neutral-700 shadow-sm"
+            />
+          )}
+        </div>
+      </section>
+    </section>
   );
 };
 
