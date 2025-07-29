233c56eb83ccf7083af872f734e8baa2f3779482
Suman Sourabh <sumsourabh14@gmail.com>
2025-07-29 16:35:46 +0530

    feat: added delete item

--- app/my-things/[id]/page.tsx
+++ app/my-things/[id]/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import DeleteIconButton from "@/components/buttons/DeleteIconButton";
+import { ItemProps } from "@/types";
 import { getDaysLeft } from "@/utils/dateFunctions";
-import { getItemFromLocalStorage } from "@/utils/localStorageFunctions";
-import { notFound } from "next/navigation";
+import {
+  addItemsToLocalStorage,
+  getItemFromLocalStorage,
+} from "@/utils/localStorageFunctions";
+import { notFound, useRouter } from "next/navigation";
 import { use } from "react";
+import { toast } from "sonner";
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -11,6 +17,7 @@ type Props = {
 
 const ThingPage = ({ params }: Props) => {
   const { id } = use(params);
+  const router = useRouter();
 
   const data = getItemFromLocalStorage(parseInt(id));
 
@@ -19,27 +26,47 @@ const ThingPage = ({ params }: Props) => {
   const { title, deadline, imageUrl } = data;
   const daysLeft = getDaysLeft(deadline);
 
+  const handleDeleteItem = (id: string) => {
+    if (typeof window == "undefined") return null;
+
+    const items = localStorage.getItem("items");
+    const parsedItems: ItemProps[] = items ? JSON.parse(items) : [];
+    const updatedItems = parsedItems.filter(
+      (item: ItemProps) => item.id !== parseInt(id)
+    );
+
+    addItemsToLocalStorage(updatedItems);
+
+    toast("Stuff has been removed.");
+    router.push("/my-things");
+  };
+
   return (
-    <section className="font-sans flex items-center justify-center px-8 py-20">
-      <section className="flex gap-16 items-center flex-col md:flex-row">
-        <div className="text-center flex-1/2">
-          <h1 className="text-8xl sm:text-9xl font-bold">{daysLeft}</h1>
-          <p className="text-xl">days to go</p>
-        </div>
-        <div className="space-y-4">
-          <h2 className="text-3xl font-semibold text-muted-foreground">
-            {title}
-          </h2>
-          {imageUrl && (
-            <img
-              src={imageUrl}
-              alt="Uploaded"
-              className="w-full max-h-[400px] object-contain rounded-lg border border-neutral-300 dark:border-neutral-700 shadow-sm"
-            />
-          )}
-        </div>
+    <>
+      <section className="font-sans flex items-center justify-center px-8 py-20 max-w-3xl mx-auto">
+        <section className="flex gap-16 items-center flex-col md:flex-row">
+          <div className="text-center flex-1/2">
+            <h1 className="text-8xl sm:text-9xl font-bold">{daysLeft}</h1>
+            <p className="text-xl">days to go</p>
+          </div>
+          <div className="space-y-4">
+            <h2 className="text-3xl font-semibold text-muted-foreground">
+              {title}
+            </h2>
+            {imageUrl && (
+              <img
+                src={imageUrl}
+                alt="Uploaded"
+                className="w-full max-h-[400px] object-contain rounded-lg border border-neutral-300 dark:border-neutral-700 shadow-sm"
+              />
+            )}
+          </div>
+        </section>
       </section>
-    </section>
+      <section className="text-center pb-8">
+        <DeleteIconButton handleDelete={() => handleDeleteItem(id)} />
+      </section>
+    </>
   );
 };
 
